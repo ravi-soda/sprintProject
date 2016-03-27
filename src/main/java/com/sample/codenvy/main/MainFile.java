658b51dd04f5ed5327944020bb46bd166ad0ce67
ravi-soda <raviprasad38@gmail.com>
2016-03-27 13:36:21 +0530

    add new syso
    update

--- src/main/java/com/sample/codenvy/main/MainFile.java
+++ src/main/java/com/sample/codenvy/main/MainFile.java
@@ -2,7 +2,8 @@ package com.sample.codenvy.main;
 
 public class MainFile {
 	public static void main(String[] args) {
-    System.out.println("ki");
-    System.out.println("ka");//jhjgjg
-}
+	    System.out.println("ki");
+	    System.out.println("and");
+	    System.out.println("ka");//jhjgjg
+	}
 }
